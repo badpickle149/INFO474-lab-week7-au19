2af94a62bad084b2021cc566be8dc625f82e505e
badpickle149 <kevinweng95@gmail.com>
2020-02-18 14:57:47 -0800

    added tooltip in viz example

--- js/viz-in-tooltip.js
+++ js/viz-in-tooltip.js
@@ -2,6 +2,15 @@
 // for more about line graphs check out this example:
 // https://bl.ocks.org/gordlea/27370d1eea8464b04538e6d8ced39e89
 
+/**
+ * changes made from linegraph.js:
+ * 1. move the code to append div to body to the beginning of the code
+ * 2. make a new svg with same dimensions and append it to div variable
+ * 3. make new axes and append it to tooltipSvg
+ * 4. change svg.append("path").... to tooltipSvg.append("path")
+ * 5. make css changes (specified in file)
+ */
+
 const margin = {top: 50, right: 50, bottom: 50, left: 50}
 , width = 800 - margin.left - margin.right // Use the window's width 
 , height = 600 - margin.top - margin.bottom // Use the window's height
@@ -9,8 +18,18 @@ const margin = {top: 50, right: 50, bottom: 50, left: 50}
 // load data
 d3.csv('data/gapminder.csv').then((data) => {
 
+    // append the div which will be the tooltip
+    // append tooltipSvg to this div
+    const div = d3.select('body').append('div')
+        .attr('class', 'tooltip')
+        .style('opacity', 0)
+
     // make an svg and append it to body
     const svg = d3.select('body').append("svg")
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+
+    const tooltipSvg = div.append("svg")
         .attr('width', width + margin.left + margin.right)
         .attr('height', height + margin.top + margin.bottom)
 
@@ -29,6 +48,10 @@ d3.csv('data/gapminder.csv').then((data) => {
         .attr("transform", "translate(0," + (height + margin.top) + ")")
         .call(d3.axisBottom(xScale))
 
+    const xAxis2 = tooltipSvg.append("g")
+        .attr("transform", "translate(0," + (height + margin.top) + ")")
+        .call(d3.axisBottom(xScale))
+
     // get min and max life expectancy for US
     const lifeExpectancyLimits = d3.extent(data, d => d['life_expectancy']) 
 
@@ -42,24 +65,23 @@ d3.csv('data/gapminder.csv').then((data) => {
         .attr("transform", "translate(" + margin.left + ",0)")
         .call(d3.axisLeft(yScale))
 
+    const yAxis2 = tooltipSvg.append("g")
+        .attr("transform", "translate(" + margin.left + ",0)")
+        .call(d3.axisLeft(yScale))
+
     // d3's line generator
     const line = d3.line()
         .x(d => xScale(d['year'])) // set the x values for the line generator
         .y(d => yScale(d['life_expectancy'])) // set the y values for the line generator 
 
     // append line to svg
-    svg.append("path")
+    tooltipSvg.append("path")
         // difference between data and datum:
         // https://stackoverflow.com/questions/13728402/what-is-the-difference-d3-datum-vs-data
         .datum(data)
         .attr("d", function(d) { return line(d) })
         .attr("fill", "steelblue")
         .attr("stroke", "steelblue")
-
-    // append the div which will be the tooltip
-    const div = d3.select('body').append('div')
-        .attr('class', 'tooltip')
-        .style('opacity', 0)
     
     // append dots to svg to track data points
     svg.selectAll('.dot').data(data)
@@ -69,13 +91,13 @@ d3.csv('data/gapminder.csv').then((data) => {
             .attr('cy', d => yScale(d['life_expectancy']))
             .attr('r', 4)
             .attr('fill', 'steelblue')
+            // get rid of .html
             .on("mouseover", function(d) {
                 div.transition()
                     .duration(200)
                     .style('opacity', 0.9)
 
-                div.html(d['life_expectancy'] + "<br/>" + d['year'])
-                    .style('left', d3.event.pageX + "px")
+                div.style('left', d3.event.pageX + "px")
                     .style('top', (d3.event.pageY - 28) + "px")
             })
             .on("mouseout", function(d) {
